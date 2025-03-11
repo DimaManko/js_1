fbbbeb3ae349265c64552dea1ee559a3217fcb6c
DimaManko <mankonat@google.com>
2025-03-11 22:32:55 +0300

    ls 2 03js

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,23 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+const score = 100 // тестовое значение, можно изменять
 let grade
 // your code
+
+if (score >= 0 && score <= 100) {
+    if (score >= 0 && score <= 49) {
+        console.log("'F'");
+    } else if (score >= 50 && score <= 69) {
+        console.log("'D'");
+    } else if (score >= 70 && score <= 79) {
+        console.log("'C'");
+    } else if (score >= 80 && score <= 89) {
+        console.log("'B'");
+    } else {
+        console.log("'A'");
+    }
+        
+    } else {
+    console.log("Введите число от 0 до 100");
+}
