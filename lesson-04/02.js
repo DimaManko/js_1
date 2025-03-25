90963cca94f183f6e3ac682e447d7b2a858b8385
DimaManko <mankonat@google.com>
2025-03-25 19:43:44 +0300

    first 02js 4less

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -14,5 +14,26 @@
 
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
+let array1 = [1, 2, 3, 2, 1, 4]
+let arrUniq = [];
+function findUniqueElements(array, uniq) {
+    
+    for(let i = 0; i < array.length; i++) {
+        let isUniq = true;
+        for (let j = 0; j < uniq.length; j++) {
+    if (array[i] === uniq[j]) {
+        isUniq = false;
+        break
+    }
+}
+    if (isUniq) {
+        uniq.push(array[i]);
+    }
 
-function findUniqueElements() {}
+    }
+    return uniq;
+}
+
+let x = findUniqueElements(array1, arrUniq);
+
+console.log(x);
